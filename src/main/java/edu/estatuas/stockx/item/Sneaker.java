1ed82a3c7c23f72db8825118dbde975c07025fec
M. Marcelo S. Salgado <mmssalgado09@gmail.com>
2025-03-13 13:45:31 +0100

    feat: :sparkles: add variables & getters/setters to Sneaker

--- src/main/java/edu/estatuas/stockx/item/Sneaker.java
+++ src/main/java/edu/estatuas/stockx/item/Sneaker.java
@@ -1,4 +1,47 @@
 package edu.estatuas.stockx.item;
 
 public class Sneaker {
+
+    private final String style;
+    private final String name;
+    private int sale;
+    private int ask;
+    private int bid;
+
+    public Sneaker(String style, String name){
+        this.style = style;
+        this.name = name;
+    }
+
+    void setSale(int sale){
+        this.sale = sale;
+    }
+
+    void setBid(int bid) {
+        this.bid = bid;
+    }
+
+    void setAsk(int ask) {
+        this.ask = ask;
+    }
+
+    String getStyle(){
+        return style;
+    }
+
+    String getName(){
+        return name;
+    }
+
+    int getSale(){
+        return sale;
+    }
+
+    int getBid(){
+        return bid;
+    }
+
+    int getAsk(){
+        return ask;
+    }
 }
