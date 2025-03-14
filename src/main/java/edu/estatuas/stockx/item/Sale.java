600b81c755312f700eda0d602979c414d1adc4a3
M. Marcelo S. Salgado <mmssalgado09@gmail.com>
2025-03-14 13:06:54 +0100

    feat(item): :sparkles: add methods to Sale

--- src/main/java/edu/estatuas/stockx/item/Sale.java
+++ src/main/java/edu/estatuas/stockx/item/Sale.java
@@ -1,4 +1,32 @@
 package edu.estatuas.stockx.item;
 
 public class Sale {
+
+    private final String size;
+    private final Integer sale;
+
+    public Sale(String size, Integer bid) {
+        this.size = size;
+        this.sale = bid;
+    }
+
+    public String size(){
+        return this.size;
+    }
+
+    public int ask(){
+        return this.sale;
+    }
+
+    public int compareTo(Offer otherOffer){
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Bid{" +
+                "size='" + size + '\'' +
+                ", bid=" + sale +
+                '}';
+    }
 }
