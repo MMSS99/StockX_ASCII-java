18dad15a69ebe6500a4ea08ac7c95d363fe8e050
M. Marcelo S. Salgado <mmssalgado09@gmail.com>
2025-03-14 13:06:17 +0100

    feat(item): :sparkles: add methods to Ask

--- src/main/java/edu/estatuas/stockx/item/Ask.java
+++ src/main/java/edu/estatuas/stockx/item/Ask.java
@@ -1,4 +1,31 @@
 package edu.estatuas.stockx.item;
 
 public class Ask {
+    private final String size;
+    private final Integer ask;
+
+    public Ask(String size, Integer bid) {
+        this.size = size;
+        this.ask = bid;
+    }
+
+    public String size(){
+        return this.size;
+    }
+
+    public int ask(){
+        return this.ask;
+    }
+
+    public int compareTo(Offer otherOffer){
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Bid{" +
+                "size='" + size + '\'' +
+                ", bid=" + ask +
+                '}';
+    }
 }
