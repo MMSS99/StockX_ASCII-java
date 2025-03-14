666c026a2cba2f35fa1481957db007f61a282806
M. Marcelo S. Salgado <mmssalgado09@gmail.com>
2025-03-14 12:54:31 +0100

    feat(item): :sparkles: implement Item inteface on Sneaker class

--- src/main/java/edu/estatuas/stockx/stockx.java
+++ src/main/java/edu/estatuas/stockx/stockx.java
@@ -1,7 +1,38 @@
 package edu.estatuas.stockx;
 
-public class stockx {
+import edu.estatuas.stockx.item.Item;
+import edu.estatuas.stockx.item.Sneaker;
+
+public class Stockx {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        /**
+         * Crear la zapatilla
+         * y printar sus datos.
+         *
+         * Lee la funcion draw() del main
+         * para conocer que propiedades
+         * de la zapatilla han de printarse.
+         */
+
+        Item sneaker = new Sneaker("555088-105", "Jordan 1 Retro High Dark Mocha");
+        System.out.println(Stockx.draw(sneaker));
+
+    }
+
+    public static String draw(Item sneaker) {
+    return
+            "\n\n\t\t" + sneaker.getAsk() + " Buy\t"
+                    + sneaker.getBid() + " Sell \n" +
+
+                    "\t\t" + " _    _" + "\n" +
+                    "\t\t" + "(_\\__/(,_" + "\n" +
+                    "\t\t" + "| \\ `_////-._" + "\n" +
+                    "\t\t" + "J_/___\"=> __/`\\" + "\n" +
+                    "\t\t" + "|=====;__/___./" + "\n" +
+                    "\t\t" + "\'-\'-\'-\"\"\"\"\"\"\"`" + "\n" +
+
+                    "\t" + sneaker.toString() + "\n" +
+                    "\t\tlast sale: " + sneaker.getSale();
     }
 }
