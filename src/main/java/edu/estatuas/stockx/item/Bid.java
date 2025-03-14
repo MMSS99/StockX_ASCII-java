6add9834b4c0379e33f563b31e26be199b1476d8
M. Marcelo S. Salgado <mmssalgado09@gmail.com>
2025-03-14 13:06:43 +0100

    feat(item): :sparkles: add methods to Bid

--- src/main/java/edu/estatuas/stockx/item/Bid.java
+++ src/main/java/edu/estatuas/stockx/item/Bid.java
@@ -1,4 +1,31 @@
 package edu.estatuas.stockx.item;
 
 public class Bid {
+    private final String size;
+    private final Integer bid;
+
+    public Bid(String size, Integer bid) {
+        this.size = size;
+        this.bid = bid;
+    }
+
+    public String size(){
+        return this.size;
+    }
+
+    public int bid(){
+        return this.bid;
+    }
+
+    public int compareTo(Offer otherOffer){
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Bid{" +
+                "size='" + size + '\'' +
+                ", bid=" + bid +
+                '}';
+    }
 }
