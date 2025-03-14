0a02e05161296fed022902ef3575d47b282cc578
M. Marcelo S. Salgado <mmssalgado09@gmail.com>
2025-03-14 13:56:51 +0100

    feat(main): :sparkles:  extend main

--- src/main/java/edu/estatuas/stockx/Stockx.java
+++ src/main/java/edu/estatuas/stockx/Stockx.java
@@ -1,5 +1,7 @@
 package edu.estatuas.stockx;
 
+import edu.estatuas.stockx.item.Ask;
+import edu.estatuas.stockx.item.Bid;
 import edu.estatuas.stockx.item.Item;
 import edu.estatuas.stockx.item.Sneaker;
 
@@ -18,6 +20,32 @@ public class Stockx {
         Item sneaker = new Sneaker("555088-105", "Jordan 1 Retro High Dark Mocha");
         System.out.println(Stockx.draw(sneaker));
 
+        /**
+         * Crear bids
+         * y añadirlas a la zapatilla
+         * en sus offers.
+         */
+
+        Bid bid = new Bid("13", 550);
+        sneaker.add(bid);
+        sneaker.add(new Bid("6", 200));
+        sneaker.add(new Bid("9.5", 479));
+        sneaker.add(new Bid("13", 338));
+        sneaker.add(new Bid("9.5", 480));
+
+        /**
+         * Crear asks
+         * y añadirlas a la zapatilla
+         * en sus offers.
+         */
+
+        Ask ask = new Ask("13", 288);
+        sneaker.add(ask);
+        sneaker.add(new Ask("6", 600));
+        sneaker.add(new Ask("9.5", 333));
+        sneaker.add(new Ask("9.5", 340));
+        sneaker.add(new Ask("13", 330));
+
     }
 
     public static String draw(Item sneaker) {
@@ -32,7 +60,7 @@ public class Stockx {
                     "\t\t" + "|=====;__/___./" + "\n" +
                     "\t\t" + "\'-\'-\'-\"\"\"\"\"\"\"`" + "\n" +
 
-                    "\t" + sneaker.toString() + "\n" +
+                    "\t" + sneaker + "\n" +
                     "\t\tlast sale: " + sneaker.getSale();
     }
 }
