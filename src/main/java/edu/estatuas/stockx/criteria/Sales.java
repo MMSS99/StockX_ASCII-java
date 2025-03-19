170c5f44db01078b472347bab36ae8360eb5d6dc
M. Marcelo S. Salgado <mmssalgado09@gmail.com>
2025-03-19 11:12:15 +0100

    feat(criteria): :sparkles: add Sales (tests added in previous commit)

--- src/main/java/edu/estatuas/stockx/criteria/Sales.java
+++ src/main/java/edu/estatuas/stockx/criteria/Sales.java
@@ -1,4 +1,14 @@
 package edu.estatuas.stockx.criteria;
 
-public class Sales {
+import edu.estatuas.stockx.item.Item;
+import edu.estatuas.stockx.item.Offer;
+
+import java.util.List;
+
+public class Sales implements Criteria{
+
+    @Override
+    public List<Offer> checkCriteria(Item sneaker) {
+        return sneaker.getOffers().stream().filter(Offer -> Offer instanceof Sales).toList();
+    }
 }
