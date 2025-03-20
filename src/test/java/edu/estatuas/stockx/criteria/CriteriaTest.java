7f5cd829405434cbdac1fbec77fc7bdccac893fa
M. Marcelo S. Salgado <mmssalgado09@gmail.com>
2025-03-20 12:30:39 +0100

    feat(criteria): :sparkles: add Min criteria & tests + removed unused dependency in tests

--- src/test/java/edu/estatuas/stockx/criteria/CriteriaTest.java
+++ src/test/java/edu/estatuas/stockx/criteria/CriteriaTest.java
@@ -1,7 +1,6 @@
 package edu.estatuas.stockx.criteria;
 
 import edu.estatuas.stockx.item.*;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -22,12 +21,12 @@ class CriteriaTest {
         testSneaker.add(new Bid("7", 4700));
         testSneaker.add(new Bid("2", 31));
         testSneaker.add(new Ask("10", 912));
-        testSneaker.add(new Ask("8", 91));
+        testSneaker.add(new Ask("10", 91));
         testSneaker.add(new Sale("14", 1100));
         testSneaker.add(new Bid("9", 29));
         testSneaker.add(new Sale("11", 180));
         testSneaker.add(new Ask("5", 45));
-        testSneaker.add(new Ask("6", 1));
+        testSneaker.add(new Ask("10", 1));
         testSneaker.add(new Bid("8", 97));
     }
 
@@ -90,5 +89,14 @@ class CriteriaTest {
         Criteria andCriteria = new AndCriteria(sizes, bids);
         List<Offer> sizebidlist = andCriteria.checkCriteria(testSneaker);
         assertEquals(List.of(testSneaker.getOffers().getLast()), sizebidlist);
+    }
+
+    @Test
+    void testMinDoubleCriteria(){
+        Criteria size = new Size("10");
+        Criteria asks = new Asks();
+        Criteria min = new Min(size, asks);
+        List<Offer> minlist = min.checkCriteria(testSneaker);
+        assertTrue(minlist.contains(testSneaker.getOffers().get(11)));
     }
 }
